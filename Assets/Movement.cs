2448cf42548121b09340771baea0874cf4fbbc95
Demetrios Vlassis <95277454+TheHero9@users.noreply.github.com>
2023-04-18 21:15:40 +0300

    Practice Movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 1000f;
+    public float upsideForce = 1000f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +19,39 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce( 0, 0, sidewaysForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(0, 0, -sidewaysForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(0, upsideForce*Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.AddForce(0, -upsideForce * Time.deltaTime, 0);
+        }
     }
 
     void OnMouseDown()
     {
-        rb.AddForce(0, 200, 500);
+        rb.AddForce(0, 0, forwardForce);
     }
 }
